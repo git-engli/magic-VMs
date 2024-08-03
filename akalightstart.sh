f2afe91327a1c8e3673c4418c486fd289212526c
kitkatty <34012208+kitkatty@users.noreply.github.com>
2024-08-04 03:52:56 +0700

    Update akalightstart.sh

--- akalightstart.sh
+++ akalightstart.sh
@@ -29,6 +29,13 @@ echo "Process 10 ended at time $(date +%T) with exit status $?"
 wait
 
 PID=$!
+echo "exec" && cp aka00.sh /root/aka00.sh & sleep 1 &
+wait $!
+echo "Process 10 ended at time $(date +%T) with exit status $?"
+wait
+
+
+PID=$!
 echo "exec" && cp aka11.sh /root/aka11.sh & sleep 1 &
 wait $!
 echo "Process 10 ended at time $(date +%T) with exit status $?"
